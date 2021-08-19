0d1468c9a0588db5acfd74d5c9fbe3bb0710d633
Easin Arafat <arafatmrx@gmail.com>
2021-08-19 20:13:46 +0600

    init

---
+++ app.js
@@ -0,0 +1,45 @@
+function update(product, price, isIncreasing) {
+  const inputBox = document.getElementById(product + "-quantity");
+
+  let productQuantity = inputBox.value;
+
+  if (isIncreasing == true) {
+    inputBox.value = parseInt(inputBox.value) + 1;
+  } else if (inputBox.value > 0) {
+    inputBox.value = parseInt(productQuantity) - 1;
+  }
+  ///update case total price
+
+  //at first caseQuantity = inputBox.value which is one > then by clicking '+'  inputBox.value = 2 > caseQuantity is still 1>
+
+  let productPrice = document.getElementById(product + "-price");
+
+  productPrice.innerText = inputBox.value * price;
+}
+
+/// update phone case price
+document.getElementById("case-plus").addEventListener("click", function () {
+  //   const caseInput = document.getElementById("case-quantity");
+  //   const caseQuantity = caseInput.value;
+  //   caseInput.value = parseInt(caseQuantity) + 1;
+  update("case", 50, true);
+});
+
+document.getElementById("case-minus").addEventListener("click", function () {
+  //   const caseInput = document.getElementById("case-quantity");
+
+  //   const caseQuantity = caseInput.value;
+
+  //   caseInput.value = parseFloat(caseQuantity) - 1;
+
+  update("case", 50, false);
+});
+
+/// update phone price
+document.getElementById("phone-plus").addEventListener("click", function () {
+  update("phone", 1150, true);
+});
+
+document.getElementById("phone-minus").addEventListener("click", function () {
+  update("phone", 1150, false);
+});
